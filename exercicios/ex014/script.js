7f225fd6f3684014fb558fe82e411b33fa0bf114
Erick <erickfsbmantel@gmail.com>
2025-02-27 21:08:42 -0300

    commit 1

--- exercicios/ex014/script.js
+++ exercicios/ex014/script.js
@@ -1,3 +1,6 @@
-function click() {
-    alert('oi');
+var valor = new Date()
+
+function meuclick() {
+    var res = document.getElementById('res')
+    res.innerHTML = `<p>O que eu recebi do sistema foi <mark>${valor}</mark></p>`
 }
---
+++ exercicios/ex015/script.js
@@ -0,0 +1,6 @@
+var objetoData = new Date()
+
+function analisar(){
+    var res = document.getElementById('resultado')
+    res.innerHTML = ``
+}
