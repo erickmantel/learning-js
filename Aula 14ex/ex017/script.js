35a5f13b14b6acf6b395d6b2a383e0bd14c05e07
Erick <erickfsbmantel@outlook.com>
2025-03-11 11:08:39 -0300

    commit 1

--- Aula 14ex/ex017/script.js
+++ Aula 14ex/ex017/script.js
@@ -1,8 +1,14 @@
-
-
 function input (){
-    var dateobj = new Date ()
-    var hora = dateobj.getHours()
-    var res = document.getElementById('res')
-    res.innerHTML(hora)
+    let numero = Number(document.getElementById('input').value)
+    
+    if(numero == 0 || numero < 0){
+        alert('{ERRO] Digite um numero válido, 0 ou numeros negativos não são permitidos!')
+    }else{
+        for(c = 1; c <= 10; c++){
+            newnumero = numero * c
+
+        }
+    }
+    //console.log(numero)
+
 }
