4b9987296c5905e32f4021b991599a53e558c8b9
Erick <erickfsbmantel@outlook.com>
2025-03-28 19:33:03 -0300

    commit 1

--- desafios/d012/script.js
+++ desafios/d012/script.js
@@ -14,15 +14,21 @@ function mudar(){
 
     output.innerHTML = ''
     if(precoAnterior>precoAtual){
-        output.innerHTML += `O produto custava R$ ${precoAnterior} e agora custa R$$ ${precoAtual}.`
-        output.innerHTML += `Hoje o preço está mais barato.`
+        output.innerHTML += `<p>O produto custava R$ ${precoAnterior} e agora custa R$ ${precoAtual}.</p>`
+        output.innerHTML += `<p>Hoje o preço está mais barato.</p>`
         output.innerHTML += `O preço caiu R$ ${precoAnterior-precoAtual} em relação ao preço anterior.`
         
         // Calculando a porcentagem de redução
         var percentualReduzido = ((precoAnterior - precoAtual) / precoAnterior) * 100;
-        output.innerHTML += ` A redução foi de ${percentualReduzido.toFixed(2)}%.`
+        output.innerHTML += `<p>A variação de ${percentualReduzido.toFixed(2)}% para baixo.<p>`
     }else{
-    
+        output.innerHTML += `<p>O produto custava R$ ${precoAnterior} e agora custa R$ ${precoAtual}.</p>`
+        output.innerHTML += `<p>Hoje o preço está mais caro.</p>`
+        output.innerHTML += `<p>O preço subiu R$ ${precoAtual-precoAnterior} em relação ao preço atual.</p>`
+       
+        // Calculando a porcentagem de redução
+        var percentualAumentado = ((precoAtual-precoAnterior) / precoAtual) * 100;
+        output.innerHTML += `<p>A variação de ${percentualAumentado.toFixed(2)}% para cima.</p>`
     }
 
 }
